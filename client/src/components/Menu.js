9cc7f4968f7f88041a1630c3bbfc658d6db73870
Seth Nakanishi <snakanishi@sandiego.edu>
2019-04-03 23:26:10 -0700

    Fixed nav bar headings

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -13,7 +13,7 @@ class Menu extends Component {
         <Navbar.Brand href="#home">Climbing-TV!</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
